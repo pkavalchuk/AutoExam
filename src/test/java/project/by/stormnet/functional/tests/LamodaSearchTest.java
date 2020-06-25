b2e841e396820e9291f54e0173bcc4d026b86681
pkavalchuk <p.kavalchuk@andersenlab.com>
2020-06-25 21:16:15 +0300

    Lesson task

--- src/test/java/project/by/stormnet/functional/tests/LamodaSearchTest.java
+++ src/test/java/project/by/stormnet/functional/tests/LamodaSearchTest.java
@@ -5,15 +5,31 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import project.by.stormnet.functional.entities.helpers.HomeHelper;
+import project.by.stormnet.functional.entities.helpers.SearchHelper;
+import project.by.stormnet.functional.entities.pages.SearchPage;
 
 public class LamodaSearchTest {
     private HomeHelper homeHelper = new HomeHelper();
+    private SearchPage searchPage = new SearchPage();
     private String searchKey = "jeans";
+    private String searchKey2 = "!@$%^";
 
     @Test
     public void performSearch(){
         int countPerPage = homeHelper.search(searchKey).getSearchResultsCountPerPage();
         Assert.assertTrue(countPerPage > 0, "No results were found!");
+    }
+
+    @Test
+    public void failedSearch(){
+        boolean countPerPage = searchPage.getElementInLamoda();
+        Assert.assertFalse(true, "No results were found!");
+    }
+
+    @Test(enabled = false)
+    public void skippedSearch(){
+        int countPerPage = homeHelper.search(searchKey).getSearchResultsCountPerPage();
+        Assert.assertTrue(countPerPage > 0, "Test was skipped!");
     }
 
     @AfterClass
